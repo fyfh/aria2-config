2f7600b9e694b24fcc993a3acb1e39aec2be373c
fyfh <13757260730@sina.cn>
2020-03-12 12:49:00 +0800

    Add files via upload

--- autoupload.sh
+++ autoupload.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
-path="$3"
-downloadpath='/usr/local/caddy/www/aria2/Download'
-rclone='mn:'   #默认根目录下
+path="$3" #取原始路径，我的环境下如果是单文件则为/data/demo.png,如果是文件夹则该值为文件夹内某个文件比如/data/a/b/c/d.jpg
+downloadpath='/usr/local/caddy/www/aria2/Download' #下载目录
+rclone='mn:自下'   #rclone挂载的目录
 
-if [ $2 -eq 0 ]
+if [ $2 -eq 0 ] #下载文件为0跳出脚本
         then
                 exit 0
 fi
 
-while true; do
+while true; do  #提取下载文件根路径，如把/data/a/b/c/d.jpg变成/data/a
     filepath=$path
     path=${path%/*}; 
     if [ "$path" = "$downloadpath" ] && [ $2 -eq 1 ]
         then
         rm '${filepath}.aria2'
-        rclone copy -vv "${filepath}" "${rclone}"
+        rclone move -v "${filepath}" "${rclone}"
         rm -rf "${filepath}"
         exit 0
-    elif [ "$path" = "$downloadpath" ]
+    elif [ "$path" = "$downloadpath" ]   #文件夹
         then
-        rm '${filepath}.aria2'
-        rclone copy -vv "${filepath}" "${rclone}/${filepath#${downloadpath}/}"
+	rm '${filepath}.aria2'
+        rclone move -v "${filepath}" "${rclone}/${filepath#${downloadpath}/}"
         rm -rf "${filepath}"
         exit 0
     fi
 done
+
